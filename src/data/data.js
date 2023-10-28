508b519cf9360cff7d3476b072d682d11937cc91
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2023-10-29 02:45:15 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -49,44 +49,7 @@ const heroapi = {
   ],
 };
 
-const popularsales = {
-  title: "Popular Sales",
-  items: [
-    {
-      id: "0p0x1",
-      title: "MiPOSSIBLE 2.0",
-      text: "MEN'S T-SHIRT",
-      rating: "4.9",
-      btn: "Buy Now",
-      img: psale2,
-      price: "200",
-      color: "from-blue-600 to-blue-500",
-      shadow: "shadow-lg shadow-blue-500",
-    },
-    {
-      id: "0p0x2",
-      title: "MiPOSSIBLE 2.0",
-      text: "MEN'S T-SHIRT",
-      rating: "4.5",
-      btn: "Buy Now",
-      img: psale1,
-      price: "200",
-      color: "from-red-500 to-rose-500",
-      shadow: "shadow-lg shadow-rose-500",
-    },
-    {
-      id: "0p0x3",
-      title: "MiPOSSIBLE 2.0",
-      text: "MEN'S T-SHIRT",
-      rating: "5+",
-      btn: "Buy Now",
-      img: psale3,
-      price: "200",
-      color: "from-violet-500 to-indigo-500",
-      shadow: "shadow-lg shadow-violet-500",
-    },
-  ],
-};
+
 
 const highlight = {
   heading: "HIGHLIGHTS",
