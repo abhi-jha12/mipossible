7dfa99d75e30114dfbb3aee745692c134e6ac674
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2023-11-03 18:31:28 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,9 @@ const Hero = ({ heroapi: { title, subtitle, btntext, btnurl, img, sociallinks, v
         <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
         <div className='relative opacity-100 z-20 grid items-center justify-items-center nike-container'>
           <div className='grid items-center justify-items-center mt-28 md:mt-24'>
-          <span className='type-moto sm:h-5 sm:w-auto text-5xl lg:text-3xl md:text-2xl sm:text-xsm xsm:text-xsm font-bold filter drop-shadow-sm text-slate-200'></span>
+          <span className='type-moto sm:h-5 sm:w-auto text-5xl lg:text-3xl md:text-2xl sm:text-xsm xsm:text-xsm text-black font-bold filter drop-shadow-sm text-slate-200'></span>
           <a href={btnurl} target={"_blank"} role="button">
-            <button type='submit' className='button-theme bg-slate-200  shadow-slate-200 rounded-xl my-5'>{btntext}</button>
+            <button type='submit' className='button-theme bg-black text-white rounded-xl my-5'>{btntext}</button>
             </a>
             <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
               {videos?.map((val, i) => (
