476fb4a9c2b00a39238b5aa58c13372729d10f6a
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2023-11-07 22:00:51 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,5 @@ export { default as Sales } from './Sales.jsx';
 export { default as FlexContent } from './FlexContent.jsx';
 export { default as Stories } from './Stories.jsx';
 export { default as Footer } from './Footer.jsx';
+export { default as Topratedsales } from './Topratedsales.jsx';
 export { default as Allproducts } from'./Allproducts.jsx';
