f8d5bd20a08fdcaf196ac6eaca28fbd76383098b
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2023-11-07 20:12:05 +0530

    Add files via upload

---
+++ src/components/Allproducts.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Item from "./utils/Item";
+import Title from "./utils/Title";
+
+const Allproducts = ({ ifExists, endpoint: { items } }) => {
+  const title = "Popular Sales";
+  return (
+    <>
+      <div className="nike-container">
+        <Title title={title} />
+        <div
+          className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-6 mb-5 ${
+            ifExists
+              ? "grid-cols-3 xl:grid-cols-2 sm:grid-cols-1"
+              : "grid-cols-3 xl:grid-cols-3 sm:grid-cols-1"
+          }`}
+        >
+          {items?.map((item, i) => (
+            <Item {...item} key={i} ifExists={ifExists} />
+          ))}
+        </div>
+      </div>
+      
+    </>
+  );
+}
+
+export default Allproducts
